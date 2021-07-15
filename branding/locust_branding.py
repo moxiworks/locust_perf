c9222636ed9e1b5db5f24f161bc39d0500e91f86
Aziz Ettobi <aziz.ettobi@moxiworks.com>
2021-07-14 19:28:45 -0700

    plugged in csv

--- branding/locust_branding.py
+++ branding/locust_branding.py
@@ -1,48 +1,51 @@
 import requests
-import csv
+import pandas as pd
 from locust import HttpUser, task, between
 
+csv_files = ['agent', 'company', 'office']
+path = 'branding/csv'
 
-csv_file = 'agent.csv'
+dataframes_list = []
 
-
-with open(csv_file, 'r') as read_obj:
-    csv_reader = csv.reader(read_obj)
-    list_of_rows = list(csv_reader)
-
-# print(list_of_rows)
-agents = [x for [x] in list_of_rows]
-print(agents)
+for i in range(len(csv_files)):
+    temp_df = pd.read_csv(f"{path}/{csv_files[i]}.csv")
+    dataframes_list.append(temp_df)
 
 
-# class PerfBranding(HttpUser):
-#     wait_time = between(0.5, 1)
-#
-#     """
-#     Flush branding cache once before running the tests
-#     """
-#     try:
-#         print("START flushing branding cache...")
-#         resp = requests.get("https://svc-qa.moxiworks.com/service/v1/branding/flush_branding")
-#         resp.raise_for_status()
-#         print("DONE flushing branding cache.\n")
-#     except requests.exceptions.HTTPError as err:
-#         raise SystemExit(err)
-#
-#     def __init__(self, parent):
-#         super(PerfBranding, self).__init__(parent)
-#         self.ag_uuid = "6e02aeec-991f-4903-93ae-280b44c8bbb6"
-#         self.comp_id = "3139213"
-#         self.office_id = "21684435"
-#
-#     @task(15)
-#     def get_agent(self):
-#         self.client.get(url=F"/service/v1/branding/agent/{self.ag_uuid}?locale=en-US", name="Agent")
-#
-#     @task(4)
-#     def get_branding(self):
-#         self.client.get(url=F"/service/v1/branding/company/{self.comp_id}?locale=en-US", name="Company")
-#
-#     @task(1)
-#     def get_office(self):
-#         self.client.get(url=F"/service/v1/branding/office/{self.office_id}?locale=en-US", name="Office")
+class PerfBranding(HttpUser):
+    wait_time = between(0.5, 1)
+
+    """
+    Flush branding cache once before running the tests
+    """
+    try:
+        print("START flushing branding cache...")
+        resp = requests.get("https://svc-qa.moxiworks.com/service/v1/branding/flush_branding")
+        resp.raise_for_status()
+        print("DONE flushing branding cache.\n")
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+    def __init__(self, parent):
+        super(PerfBranding, self).__init__(parent)
+        self.agents = dataframes_list[0]
+        self.companies = dataframes_list[1]
+        self.offices = dataframes_list[2]
+
+    @task(15)
+    def get_agent(self):
+        for _, ag in self.agents.iterrows():
+            a = self.client.get(url=F"/service/v1/branding/agent/{ag[0]}?locale=en-US", name="Agents")
+            a.raise_for_status()
+
+    @task(4)
+    def get_companies(self):
+        for _, br in self.companies.iterrows():
+            a = self.client.get(url=F"/service/v1/branding/company/{br[0]}?locale=en-US", name="Companies")
+            a.raise_for_status()
+
+    @task(1)
+    def get_office(self):
+        for _, off in self.offices.iterrows():
+            a = self.client.get(url=F"/service/v1/branding/office/{off[0]}?locale=en-US", name="Offices")
+            a.raise_for_status()
--- load_branding_test.sh
+++ load_branding_test.sh
@@ -1,2 +1,2 @@
-locust -f branding/locust_branding.py --host https://svc-qa.moxiworks.com --users 400 --spawn-rate 20 --csv \
+locust -f branding/locust_branding.py --host https://svc-qa.moxiworks.com --users 300 --spawn-rate 20 --csv \
 branding/reports/branding
