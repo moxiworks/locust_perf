3a908bf30688ec05b45d9ab589ec5a260d946aed
Alec Blanton <alecblanton@MBP-0707.local>
2024-05-09 11:14:59 -0700

    alt office 2

--- load_v2_ag_alt_office_test.sh
+++ load_v2_ag_alt_office_test.sh
@@ -1,2 +1,2 @@
-locust -f wms_svc_profile/locust_v2_insecure_comp_agents.py --web-host=localhost --host https://svc-st.moxiworks.com --users 25 \
+locust -f wms_svc_profile/locust_v2_alt_office.py --web-host=localhost --host https://svc-st.moxiworks.com --users 25 \
 --spawn-rate 2
--- wms_svc_profile/data.py
+++ wms_svc_profile/data.py
@@ -3,6 +3,16 @@ from faker import Faker
 mock = Faker()
 
 v2_insec_comp_ag_endpoint = "/service/profile/v2_insecure/company/3139213/agents"
+v2_endpoint = "/service/v1/profile/"
+
+alt_off_ag_uuids = [
+    "9d692c32-4182-4431-8ffc-ae02ef5bd19c",
+    "bd8ea968-4737-4d21-b937-18f6851f5a3d",
+    "373093d2-19fa-4b97-a451-2f2871875377",
+    "a5b7a699-ea7c-4429-8f8e-e6b00ce02a1f",
+    "740e315a-8957-464c-be2f-3cd2b15d69ea",
+    "7654c231-578d-4ca5-96be-8e28cc0be27c"
+]
 
 auth = {
     "testing": "ST"
